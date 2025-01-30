ec9189da83f6f8d17fcf518b13d6ea3685a4688f
Loetgott <146114273+Loetgott@users.noreply.github.com>
2025-01-30 16:03:06 +0100

    port selection works

--- src/RopewayControl/Main.java
+++ src/RopewayControl/Main.java
@@ -1,9 +1,73 @@
 package RopewayControl;
 
-import RopewayGUI.*;
+import com.fazecast.jSerialComm.SerialPort;
+
+import java.io.IOException;
+import RopewayGUI.GUI;
 
 public class Main {
+    static SerialPort port;
     public static void main(String[] args) {
         GUI gui = new GUI();
+
+        // Nachrichten vom Arduino empfangen
+        //StringBuilder messageBuilder = new StringBuilder();
+        //while (true) {
+        //    if (port.bytesAvailable() > 0) {
+        //        byte[] buffer = new byte[1]; // Ein Byte lesen, um Zeichen zu dekodieren
+        //        int bytesRead = 0;
+        //        try {
+        //            bytesRead = port.getInputStream().read(buffer);
+        //        } catch (IOException e) {
+        //            throw new RuntimeException(e);
+        //        }
+        //        if (bytesRead > 0) {
+        //            char receivedChar = (char) buffer[0];
+        //            if (receivedChar == '\n') { // Nachricht ist vollständig
+        //                String receivedMessage = messageBuilder.toString();
+        //                System.out.println("Empfangen: " + receivedMessage);
+        //                messageBuilder.setLength(0); // Nachricht löschen
+        //            } else {
+        //                messageBuilder.append(receivedChar);
+        //            }
+        //        }
+        //    }
+        //}
+    }
+
+    public static SerialPort[] getPorts(){
+        return SerialPort.getCommPorts();
+    }
+
+    public static void setOutputPort(String portName){
+        port = SerialPort.getCommPort(portName.split("\\(")[1].split("\\)")[0]);
+        port.setComPortParameters(9600, 8, 1, 0); // Baudrate, Datenbits, Stoppbits, Parität
+        port.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 1000, 1000); // Timeout auf 1 Sekunde setzen
+
+        if (port.openPort()) {
+            System.out.println("Port geöffnet!");
+            GUI.setButtonsEnabled(true);
+        } else {
+            System.err.println("Fehler beim Öffnen des Ports.");
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            if (port.isOpen()) {
+                port.closePort();
+                System.out.println("Port geschlossen.");
+                GUI.setButtonsEnabled(false);
+            }
+        }));
+    }
+
+    public static void sendData(String message){
+        try {
+            port.getOutputStream().write(message.getBytes());
+            port.getOutputStream().flush();
+            System.out.println("Nachricht gesendet: " + message);
+        } catch (IOException e) {
+            System.err.println("Fehler beim Senden der Nachricht.");
+            e.printStackTrace();
+        }
     }
 }
--- src/RopewayGUI/GUI.java
+++ src/RopewayGUI/GUI.java
@@ -1,13 +1,18 @@
 package RopewayGUI;
 
+import RopewayControl.Main;
+import com.fazecast.jSerialComm.SerialPort;
 import com.formdev.flatlaf.FlatDarkLaf;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class GUI {
     public GUI() {
         try {
             UIManager.setLookAndFeel(new FlatDarkLaf());
+            UIManager.put("Menu.selectionBackground", new Color(83, 83, 83));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -17,11 +22,41 @@ public class GUI {
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         JMenuBar menuBar = new JMenuBar();
-        JMenu fileMenu = new JMenu("Datei");
-        menuBar.add(fileMenu);
+        JMenu portMenu = new JMenu("Port");
+        updateComPorts(portMenu);
+        menuBar.add(portMenu);
+        JMenu updateMenu = new JMenu("Update");
+        updateMenu.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                updateComPorts(portMenu);
+            }
+        });
+        menuBar.add(updateMenu);
 
         frame.setJMenuBar(menuBar);
 
         frame.setVisible(true);
+    }
+
+    public static void updateComPorts(JMenu portMenu){
+        portMenu.removeAll();
+        SerialPort[] ports = Main.getPorts();
+        for(int i = 0; i < ports.length; i++){
+            if (!ports[i].getDescriptivePortName().split("\\(")[0].trim().equals("Standardmäßgige Seriell-über-Bluetooth-Verbindung")) {
+                JMenuItem portMenuItem = new JMenuItem(ports[i].getDescriptivePortName());
+                portMenuItem.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseReleased(MouseEvent e) {
+                        Main.setOutputPort(portMenuItem.getText());
+                    }
+                });
+                portMenu.add(portMenuItem);
+            }
+        }
+    }
+
+    public static void setButtonsEnabled(boolean enabled){
+
     }
 }
